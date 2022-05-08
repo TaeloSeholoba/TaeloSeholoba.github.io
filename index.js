eedccc51e86a49b1037f05402ecef8adce7bc462
Taelo Seholoba <tseholoba2@gmail.com>
2022-05-08 22:16:51 +0200

    modified

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const hide = () => {
   document.getElementsByTagName("nav")[0].style.animationName = "hide";
+  setTimeout(()=>{document.getElementsByClassName("menu")[0].style.visibility = "visible"},500)
 }
 
 const show = () => {
-  document.getElementsByTagName("nav")[0].style.animationName = "show"
-
+  const navStyle = document.getElementsByTagName("nav")[0].style;
+  navStyle.display = "block";
+  navStyle.animationName = "show";
+  document.getElementsByClassName("menu")[0].style.visibility = "hidden"
+  
 }
